41509f1fc2a50b5dcdd1de58bb0c857bdcd8b00f
Dim <dimcegnlm@gmail.com>
2026-03-11 23:45:10 +0200

    bugfix: add repo to sys path so test can import frames_to_video

--- test/test_real_music_feature_video.py
+++ test/test_real_music_feature_video.py
@@ -25,6 +25,7 @@ from typing import Dict, Optional
 from PIL import Image
 
 ROOT = Path(__file__).parent
+sys.path.insert(0, str(ROOT.parent))
 sys.path.insert(0, str(ROOT.parent / "src"))
 
 from audio_feature_extractor import AudioFeatureExtractor
